c2d94b5ac775186b14b375b6aa0d41e2e8818746
Maxsim Zhuk <maxsimzhuk2011@gmail.com>
2017-10-10 23:00:28 +0300

    Add Shoot and Heales!

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Soldier : MonoBehaviour {
+
+    public GUISkin MoneySkin;
     public GameObject Target;        //Point of the player's touch
     public GameObject HeadSoldier;  
     public GameObject RedRay;
@@ -10,43 +12,76 @@ public class Soldier : MonoBehaviour {
 
     public Texture2D RightBatton;
     public Texture2D LeftBatton;
+    public Texture2D ReloadBatton;
+    public Texture2D KickOffBatton;
+    public Texture2D ChangeBatton;
+    public Texture2D HealesTexture2D;
+    public Texture2D AmmoTexture2D;
+
+    public int fireDelays;
+    public int healse;
+    public int ammoMachineGun;
+
+    private int myAmmo;
+    private int myfireDelays;
+
+    public List<GameObject> ZombiesArmy = new List<GameObject>();
     // Use this for initialization
     void Start () {
-		
-	}
+        myAmmo = ammoMachineGun;
+        myfireDelays = fireDelays;
+    }
 	
 	// Update is called once per frame
-	void FixedUpdate() {
+	void Update() {
         RaycastHit info;
+
         float lengthRedRay;
 
+	    for (int i = 0; i < ZombiesArmy.Count; i++)
+	    {
+	        if (ZombiesArmy[i] != null)
+	        {
+	            if (ZombiesArmy[i].transform.position.x< transform.position.x +5)
+	            {
+	                ZombiesArmy[i].BroadcastMessage("AmHere", gameObject);
+	            }
+            }
+        }
+        
 	    if (5 == Random.Range(1, 200))
 	    {
 	        GameObject NewSoldier;
-            
             NewSoldier = Instantiate(ZombieGameObject, transform.position + Vector3.right * 30 - Vector3.forward * 2 - Vector3.up * (float)Random.Range( 1, 27)/9, transform.rotation);
-	    }
+            ZombiesArmy.Add(NewSoldier);
+        }
+
 	    if (Physics.Raycast(HeadSoldier.transform.position, HeadSoldier.transform.forward, out info, 30))
 	    {
-
             lengthRedRay= Vector3.Distance(HeadSoldier.transform.position, info.collider.gameObject.transform.position);
-           // info.collider.gameObject.BroadcastMessage("HitForMe");
+         
         }
         else
         {
             lengthRedRay = 30;
         }
         RedRay.transform.localScale = new Vector3(lengthRedRay, 0.1f, 0.1f);
-        RedRay.transform.position = HeadSoldier.transform.position+ HeadSoldier.transform.forward * (float)lengthRedRay/2 + HeadSoldier.transform.right * (float)0.5; 
-        
+        RedRay.transform.position = HeadSoldier.transform.position+ HeadSoldier.transform.forward * (float)lengthRedRay/2 + HeadSoldier.transform.right * (float)0.5;
+
+        if(myfireDelays > fireDelays) {} else myfireDelays++;
 
         for (int i = 0; i < Input.touchCount; i++) 
 	    {
 	        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
 
-	        if (pos.x > transform.position.x + 3)  
+            
+
+            if (pos.x > transform.position.x + 3 && myfireDelays > fireDelays)
 	        {
-	            if (Physics.Raycast(HeadSoldier.transform.position, HeadSoldier.transform.forward, out info, 30))
+                myfireDelays=0;
+                   myAmmo--;
+
+                if (Physics.Raycast(HeadSoldier.transform.position, HeadSoldier.transform.forward, out info, 30))
 	            {
                     info.collider.gameObject.BroadcastMessage("HitForMe");
 
@@ -71,8 +106,11 @@ public class Soldier : MonoBehaviour {
         }
 
 	}
-	
 
+    void HealthDamage()
+    {
+        healse --;
+    }
     void OnGUI()
     {
         if (GUI.Button(new Rect(0,0, Screen.height / 5, Screen.height / 5), "exit"))
@@ -80,10 +118,21 @@ public class Soldier : MonoBehaviour {
             Application.Quit();
         }
 
-        GUI.DrawTexture(new Rect(Screen.height / 5, Screen.height- Screen.height /5 , Screen.height / 5, Screen.height / 5), RightBatton);
+        GUI.DrawTexture(new Rect(Screen.height / 5, Screen.height - Screen.height / 5, Screen.height / 5, Screen.height / 5), RightBatton);
+        
+
         GUI.DrawTexture(new Rect(0, Screen.height - Screen.height / 5, Screen.height / 5, Screen.height / 5), LeftBatton);
-       // if (GUI.Button(new Rect(Screen.height / 5, Screen.height - Screen.height / 5, Screen.height / 5, Screen.height / 5), RightBatton)
-       // if (GUI.Button(new Rect(0, Screen.height - Screen.height / 5, Screen.height / 5, Screen.height / 5), LeftBatton)
+        GUI.DrawTexture(new Rect(0, Screen.height - Screen.height / 5*2, Screen.height / 5, Screen.height / 5), ChangeBatton);
+        GUI.DrawTexture(new Rect(0, Screen.height - Screen.height / 5 * 3, Screen.height / 5, Screen.height / 5), KickOffBatton);
+        GUI.DrawTexture(new Rect(0, Screen.height - Screen.height / 5 * 4, Screen.height / 5, Screen.height / 5), ReloadBatton);
+        for (int i = 0; i < healse; i++)
+        {
+            GUI.DrawTexture(new Rect(Screen.height / 20*i, 0, Screen.height / 20, Screen.height / 20), HealesTexture2D);
+        }
+        GUI.skin = MoneySkin;
+        GUI.Label(new Rect(0, Screen.height / 20, 700, 70), "AMMO= " + myAmmo);
+        // if (GUI.Button(new Rect(Screen.height / 5, Screen.height - Screen.height / 5, Screen.height / 5, Screen.height / 5), RightBatton)
+        // if (GUI.Button(new Rect(0, Screen.height - Screen.height / 5, Screen.height / 5, Screen.height / 5), LeftBatton)
     }
 }
 
--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
+    private GameObject Player;
+    enum Direction
+    {
+        Run,
+        Beat
+
+    }
+ private Direction _myDirection;
     public GameObject Head;
     public GameObject Body;
     public GameObject leg;
@@ -13,20 +21,70 @@ public class Zombie : MonoBehaviour
     public GameObject AnimationHit;
     public GameObject AnimationRun;
 
+    public int fireDelays;
     private float health;
     private bool alive;
     private int deadtimer;
-   // Use this for initialization
-   void Start()
+    private int myfireDelays;
+
+    // Use this for initialization
+    void Start()
     {
         alive = true;
         health = 12.0f;
-        deadtimer = 50;
+        deadtimer = 45;
+       _myDirection = Direction.Run;
+        myfireDelays = fireDelays;
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(alive)transform.position = transform.position - Vector3.right*(float) 0.1;
+        if (alive)
+        {
+
+            if (Player != null)
+            {
+                if (transform.position.x > Player.transform.position.x + 3 && _myDirection == Direction.Run)
+                {
+                    transform.position = transform.position - Vector3.right*(float) 0.1;
+                }
+
+                if (transform.position.x <= Player.transform.position.x + 3 && _myDirection == Direction.Run)
+                {
+                    _myDirection = Direction.Beat;
+                    Destroy(MyAnimation);
+                    MyAnimation = Instantiate(AnimationHit, transform.position, transform.rotation);
+                    MyAnimation.transform.SetParent(transform);
+
+                }
+                if ( _myDirection == Direction.Beat)
+                {
+                    myfireDelays--;
+                    if (myfireDelays < 0)
+                    {
+                        if (transform.position.x > Player.transform.position.x + 4 && _myDirection == Direction.Beat)
+                        {
+                            _myDirection = Direction.Run;
+                            Destroy(MyAnimation);
+                            MyAnimation = Instantiate(AnimationRun, transform.position, transform.rotation);
+                            MyAnimation.transform.SetParent(transform);
+                        }
+                        else
+                        {
+                            Player.BroadcastMessage("HealthDamage");
+                        }
+                        myfireDelays = fireDelays;
+                    }
+                    
+
+                }
+
+            }
+            else
+            {
+                transform.position = transform.position - Vector3.right * (float)0.1;
+            }
+        }
         else
         {
             deadtimer --;
@@ -40,7 +98,6 @@ public class Zombie : MonoBehaviour
 
     void HealthDamage(int damage)
     {
-
         health = health - damage;
         Myhealth.transform.localScale = new Vector3(health/4, 0.2f, 0.2f);
         if (health < 1 && alive)
@@ -53,5 +110,10 @@ public class Zombie : MonoBehaviour
             Destroy(Body);
             Destroy(leg);
         }
+    }
+
+    void AmHere(GameObject P)
+    {
+        Player = P;
     }
 }
